893b018cbca7dcbbb1a715e80f06ba3c3f2db19a
Rivka Beck <rivkabeck@Rivkas-MacBook-Pro-2.local>
2019-09-26 11:25:29 +0300

    merge commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.selenium.Eyes;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -10,6 +11,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         Eyes eyes = new Eyes();
         eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
+        String batchName = null;
+        String batchId   = System.getenv("APPLITOOLS_BATCH_ID");
+
+        // set the batch
+        BatchInfo batchInfo = new BatchInfo(batchName);
+//        batchInfo.setId(batchId);
+        eyes.setBatch(batchInfo);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         RemoteWebDriver driver = new ChromeDriver(options);
@@ -17,7 +25,7 @@ public class HelloWorld {
         eyes.open(driver,"Jenkins/Github","Jenkins/Github");
         System.out.println("Hello World");
         eyes.checkWindow("Initial view");
-        System.out.println("hi");
+        System.out.println("This is a merge print");
         driver.quit();
         eyes.close();
 
